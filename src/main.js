9e02203dedf13ae47b813d7a9c2773aa541dbe46
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-01-07 10:43:27 -0300

    Instalado dayjs

--- src/main.js
+++ src/main.js
@@ -8,3 +8,13 @@ import "./styles/scheduling.css"
 import "./styles/inputs.css"
 import "./styles/aside.css"
 import "./styles/alertLoad.css"
+
+// DAYJS
+import "./libs/dayjs"
+
+
+// import dayjs from "dayjs"
+// console.log(dayjs().format("DD-MM-YY"))
+
+//npm run server
+//npm run dev
