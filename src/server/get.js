b6edbf6a5d22f131459dd988560d35a859c3fe4d
RicardoTavaresDias <ricardo.ti.sistemas@gmail.com>
2025-01-10 02:03:09 -0300

    removendo comentarios no arquivo get

--- src/server/get.js
+++ src/server/get.js
@@ -14,16 +14,6 @@ export async function get(value) {
   }
 }
 
-const teste = await get(document.querySelector("#date").value)
-
-
-
-
-
-//const tes = teste.filter(ola => ola.name)
-
-//console.log(tes)
-
 
 
 
